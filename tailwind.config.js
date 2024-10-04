de5c33369bd8d91ca4c2023aae99477c5e54b1e1
Fardin Ahsan <md.fardin.ahsan@g.bracu.ac.bd>
2024-10-05 04:49:15 +0600

    seventh commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,3 +6,21 @@ module.exports = {
     },
     plugins: [],
   }
+
+
+  module.exports = {
+    theme: {
+      extend: {
+        keyframes: {
+          fadeIn: {
+            '0%': { opacity: 0, transform: 'translateY(100px)', filter: 'blur(33px)' },
+            '100%': { opacity: 1, transform: 'translateY(0)', filter: 'blur(0)' },
+          },
+        },
+        animation: {
+          'fade-in': 'fadeIn 1s ease-in-out forwards',
+        },
+      },
+    },
+    plugins: [],
+  };
